6ced12b009bc8bc8cd4219ce5868ca560c519752
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 17:11:59 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// 115
+// use a switch statment to log the dy of the week base on (1-7)
+var day = 1;
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid day");
+        break;
+}
---
+++ index.ts
@@ -0,0 +1,32 @@
+// 115
+// use a switch statment to log the dy of the week base on (1-7)
+
+
+let day: number = 1;
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid day");
+        break;
+}
